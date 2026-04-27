10c0e25d9ca7b67413b73477ffd30b91435136af
Emma <eb0980835@otc.edu>
2026-04-27 11:22:59 -0500

    Initial Commit

---
+++ Anime API Project/Anime_API_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anime_API_ProjectApp.swift
+//  Anime API Project
+//
+//  Created by E Blaylock on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Anime_API_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
